d77287d4eb67943d19dff84ea96c1fe72cc96cc0
Vyacheslav <gracerpro@gmail.com>
2016-01-06 12:45:46 +0300

    Add dumper dunctions

--- dumper.lua
+++ dumper.lua
@@ -1,27 +1,138 @@
 local chardumps = chardumps;
 local dumper = {
-	data = {},
-	
-	entitiesData = {},
+  -- each field are entity name
+	dump = {},
 };
 
 ---
 -- @param table options What is saved
-function dumper:dump(options)
-  self.data = {};
+function dumper:Dump(options)
+  self.dump = {};
   
   
 end
 
 ---
 -- Get entity field
-function dumper:getEntity(name)
-  if self.data[name] ~= nil then
-    return self.data[name];
+function dumper:GetEntity(name)
+  if self.dump[name] ~= nil then
+    return self.dump[name];
   end
 end
 
+function dumper:Clear()
+  self.dump = {};
+end
+
+function dumper:DeleteEntityData(name)
+  self.dump[name] = nil;
+end
+
 function dumper:Init()
+  self:Clear();
+end
+
+function dumper:GetAchievementData()
+
+end
+
+function dumper:GetActionData()
+
+end
+
+function dumper:GetBagData()
+
+end
+
+function dumper:GetBankData()
+
+end
+
+function dumper:GetBindData()
+
+end
+
+function dumper:GetCriteriasData()
+
+end
+
+function dumper:GetCritterData()
+
+end
+
+function dumper:GetCurrencyData()
+
+end
+
+function dumper:GetGlobalData()
+
+end
+
+function dumper:GetGlyphData()
+
+end
+
+function dumper:GetEquipmentData()
+
+end
+
+function dumper:GetInventoryData()
+
+end
+
+function dumper:GetMountData()
+
+end
+
+function dumper:GetPlayerData()
+
+end
+
+function dumper:GetPetData()
+
+end
+
+function dumper:GetPMmacroData()
+
+end
+
+function dumper:GetQuestData()
+
+end
+
+function dumper:GetQuestlogData()
+
+end
+
+function dumper:GetReputationData()
+
+end
+
+function dumper:GetSkillData()
+
+end
+
+function dumper:GetSkillspellData()
+
+end
+
+function dumper:GetSpellData()
+
+end
+
+function dumper:GetStatisticData()
+
+end
+
+function dumper:GetTalentData()
+
+end
+
+function dumper:GetTaxiData()
+
+end
+
+function dumper:GetTitleData()
 
 end
 
