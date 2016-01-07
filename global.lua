28a4f1cb560aa00eefedf1963a58debb5ac62438
Vyacheslav <gracerpro@gmail.com>
2016-01-07 22:07:45 +0300

    Clean global file

--- global.lua
+++ global.lua
@@ -1,13 +1,1 @@
 chardumps = chardumps or {};
-
-local _, clientBuild = GetBuildInfo();
-clientBuild = tonumber(clientBuild);
-
--- define client build constant
-if clientBuild >= 6080 and clientBuild <= 8478 then
-	WOW2 = true;
-elseif clientBuild >= 9056 and clientBuild <= 12340 then
-	WOW3 = true;
-elseif clientBuild >= 13164 and clientBuild <= 15595 then
-	WOW4 = true;
-end
