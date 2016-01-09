48128925ca6810bea659f91313a00489bb263ef5
Vyacheslav <gracerpro@gmail.com>
2016-01-09 15:05:47 +0300

    Add register all events button

--- main_frame2.lua
+++ main_frame2.lua
@@ -36,6 +36,21 @@ function mainFrame:CreateEntityFrame(name, parent)
   return frame;
 end
 
+function mainFrame:RegisterWorkEvents(frame)
+  if not frame then
+    frame = self.frame;
+  end
+  local events = {
+    "TAXIMAP_OPENED", "VARIABLES_LOADED", "BANKFRAME_OPENED", "PLAYER_LEAVING_WORLD",
+    "TRADE_SKILL_SHOW", "QUEST_DETAIL", "QUEST_PROGRESS", "QUEST_AUTOCOMPLETE",
+    "QUEST_COMPLETE", "QUEST_QUERY_COMPLETE", "ADDON_LOADED", "PLAYER_LOGOUT", -- UNIT_QUEST_LOG_CHANGED
+  }
+  frame:UnregisterAllEvents();
+  for _, name in pairs(events) do
+    frame:RegisterEvent(name);
+  end
+end
+
 function mainFrame:Init()
   local L = chardumps:GetLocale();
   local widgets = chardumps.widgets;
@@ -105,6 +120,17 @@ function mainFrame:Init()
   self.chbCrypt:ClearAllPoints();
   self.chbCrypt:SetPoint("BOTTOMRIGHT", -10 - 5 - 300, 10);
 
+  self.chbEvents = widgets:CreateCheckbox(frame, {name = "chbEvents", cx = 14, cy = 14, text = "Events"});
+  self.chbEvents:ClearAllPoints();
+  self.chbEvents:SetPoint("BOTTOMRIGHT", -10 - 5 - 370, 10);
+  self.chbEvents:SetScript("OnClick", function ()
+    if mainFrame.chbEvents:GetChecked() then
+      mainFrame.frame:RegisterAllEvents();
+    else
+      mainFrame:RegisterWorkEvents();
+    end
+  end);
+
   -- Checkbox list
 
   -- checkbox
@@ -169,14 +195,7 @@ function mainFrame:Init()
   local btn = CHD_CreateButton("btnQuestQuery", 180, chbHeight * 9 + 8, 150, btnHeight, frame);
   btn:SetScript("OnClick", CHD_OnQueryQuestClick);
 --]]
-  local events = {
-    "TAXIMAP_OPENED", "VARIABLES_LOADED", "BANKFRAME_OPENED", "PLAYER_LEAVING_WORLD",
-    "TRADE_SKILL_SHOW", "QUEST_DETAIL", "QUEST_PROGRESS", "QUEST_AUTOCOMPLETE",
-    "QUEST_COMPLETE", "QUEST_QUERY_COMPLETE", "ADDON_LOADED", "PLAYER_LOGOUT", -- UNIT_QUEST_LOG_CHANGED
-  }
-  for _, name in pairs(events) do
-    frame:RegisterEvent(name);
-  end
+  self:RegisterWorkEvents(frame);
 
   self.frameMin = frameMin;
   self.frame = frame;
@@ -345,6 +364,7 @@ function mainFrame:SetEntityChecked(name, value)
 end
 
 function mainFrame:OnEvent(event, ...)
+  chardumps.log:Debug(event);
   if "BANKFRAME_OPENED" == event then
     local bankData = {};
     if mainFrame:IsEntityChecked("bank") then
@@ -367,17 +387,20 @@ function mainFrame:OnEvent(event, ...)
     if chardumps:GetPatchVersion() <= 3 then
       return
     end
-    local questTable = GetQuestsCompleted(nil);
+    local L = chardumps:GetLocale();
+    local questIds = chardumps.dumper:GetQuestDataReal();
     local questId = GetQuestID();
     local s = L.Quest .. "(ID = " .. questId .. ")";
-    if questTable[questId] ~= nil then
+    if questIds[questId] then
       s = s .. " \124cFF00FF00 " .. L.QuestWasCompleted  .. "\r";
     end
     chardumps.log:Message(s);
   elseif "QUEST_COMPLETE" == event then
+    print(chardumps:GetPatchVersion());
     if chardumps:GetPatchVersion() <= 3 then
       return
     end
+    local L = chardumps:GetLocale();
     local questId = GetQuestID();
     chardumps.log:Message(L.Quest .. " (ID = " .. questId .. ") \124cFF00FF00 " .. L.QuestCompleted .. "\r");
   elseif "QUEST_AUTOCOMPLETE" == event then
@@ -520,13 +543,10 @@ function mainFrame:OnAddonLoaded(addonName)
 	if addonName ~= "chardumps" then
 	  return
 	end
-	chardumps.log:Debug("ADDON_LOADED");
 end
 
 function mainFrame:OnPlayerLeavingWorld()
   local options = chardumps.options;
-
-  chardumps.log:Debug("PLAYER_LEAVING_WORLD")
 
   local entities = {};
   for name, data in pairs(self.entitiesData) do
@@ -547,8 +567,6 @@ end
 
 function mainFrame:OnVariablesLoaded()
   CHD_CLIENT = {};
-
-  chardumps.log:Debug("VARIABLES_LOADED");
 
   local playerOptions = chardumps:GetPlayerOptions();
   chardumps.dumper:SetDynamicDataAll(playerOptions.dynamicData);
